eb17920a8644f837e5a57491e2b143b443471c0c
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-05-03 11:07:20 +0100

    updated npm scripts with build-prod and build-dev

---
+++ webpack.prod.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    devtool: 'source-map',
+    //stats: 'verbose', // Using verbose here means to output everything
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+    ]
+}
