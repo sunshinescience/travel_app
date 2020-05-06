78e96209e26d745ef39988aec71d372b3492c202
Sunshine Abbott <sunshine.abbott@gmail.com>
2020-05-06 21:15:37 +0100

    refactored performAction function to call two API's with a .then in order to get data from both

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -10,8 +10,77 @@ const weatherBaseURL = 'https://api.weatherbit.io/v2.0/forecast/daily?';
 const pixaAPI = '16361458-dcb8c58ed9a7618589e0d8461';
 const pixaBaseURL = 'https://pixabay.com/api/?';
 
+
+// performAction is a function that uses the geonames API and gets a city from the user and shows on the page the country, latitude and longitude
+function performAction(e){
+    console.log("performAction (0): entering");
+    const city =  document.getElementById('zip').value;
+    console.log("performAction (1): read zip:", city);
+
+    // Perhaps try calling getDate() here? To postData all together???
+    //console.log("getDate called within performAction: ", getDate());
+    console.log("performAction (2): kicking off geoname");
+    getGeoname(geonamesURL, city, userName) // The action we want to do here is call this getGeoname function
+    // Chain another Promise that makes a POST request to store all the API data we received, as well as data entered by the user, locally in the app
+    .then(function(geoResponse){ // Use the syntax 'then' to chain actions, with fetch calls
+        try{
+            // Getting the first object in the array postalCodes (from the geonames API as defined in the getName function fetch call)
+            let geoData = geoResponse.postalCodes[0];  // best match
+            console.log("getGeoname then-0 (0): received best match geodata from api: ", geoData);
+            document.getElementById('city').innerHTML = geoData.placeName;
+
+            console.log("getGeoname then-0 (1): kicking off weather forecast", city);
+            weatherForecast(city, weatherBaseURL, weatherAPI)
+            .then(function(weatherResponse){
+                console.log("weatherForecast then-0 (0): received weather forecast", weatherResponse);
+                // update weather ui stuff
+
+                // call pixabay, and in its then update pixa ui elements
+
+            });
+        }
+        catch(error){
+            console.log(error);
+        }
+        
+
+        // Add data to POST request
+        /*
+        postData('http://localhost:8080/add', {
+            city: geoData.placeName, // City name 
+            country: geoData.countryCode, // Country code
+            latitude: geoData.lat, // Latitude
+            longitude: geoData.lng // Longitude
+        }, updateUI); // Note that this has to happen once ALL of the Promises have been resolved
+        */
+        //console.log("performAction (2): postData() called - country, lat, long, data added to post response");
+        //console.log("performAction (3): updateUI() was called - the UI is being updated");
+        console.log("getGeoname then-0 (2): completed");
+    });
+
+    console.log("performAction (3): completed");
+};
+
+
+// getName is an asynchronous function that uses fetch() to make a GET request to the geonames API. This function takes three parameters, which are the base URL, the zip code we want, and the API key
+const getGeoname = async (geonamesURL, city, userName)=>{
+    console.log("getGeoname (0): started, calling fetch");
+    const res = await fetch(`${geonamesURL}placename=${city}&username=${userName}`); // We set a variable to hold the fetch calls. And the await keyword is telling it not to go on to the next part until it has received the data it needs. This URL in the fetch is what will let us query the OpenWeatherMap API. I set it so that us zip codes are hard coded
+    try {
+        const data = await res.json();
+        //console.log(data); // Printing the data in the console received from the API, based on the city the user input
+        console.log("getGeoname (1): obtained data from the geonames API");
+        return data;
+    }  
+    catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+};
+
 // Add a callback to postData - and this will be the updateUI() function
 const postData = async (url = '', data = {}, callback) => { 
+    console.log("postData (0): sending data to backend");
     const response = await fetch(url, {
         method: 'POST', 
         credentials: 'same-origin',
@@ -33,48 +102,6 @@ const postData = async (url = '', data = {}, callback) => {
     };
 };
 
-// performAction is a function that uses the geonames API and gets a city from the user and shows on the page the country, latitude and longitude
-function performAction(e){
-    const city =  document.getElementById('zip').value;
-
-    // Perhaps try calling getDate() here? To postData all together???
-    //console.log("getDate called within performAction: ", getDate());
-    
-    getGeoname(geonamesURL, city, userName) // The action we want to do here is call this getGeoname function
-    // Chain another Promise that makes a POST request to store all the API data we received, as well as data entered by the user, locally in the app
-    .then(function(data){ // Use the syntax 'then' to chain actions, with fetch calls
-        
-        // Getting the first object in the array postalCodes (from the geonames API as defined in the getName function fetch call)
-        console.log("performAction (1): User entered the place name of: ", data.postalCodes[0].placeName);
-    
-        // Add data to POST request
-        
-        postData('http://localhost:8080/add', {
-            city: data.postalCodes[0].placeName, // City name 
-            country: data.postalCodes[0].countryCode, // Country code
-            latitude: data.postalCodes[0].lat, // Latitude
-            longitude: data.postalCodes[0].lng // Longitude
-        }, updateUI()); // Note that this has to happen once ALL of the Promises have been resolved
-        
-        console.log("performAction (2): postData() called - country, lat, long, data added to post response");
-        //console.log("performAction (3): updateUI() was called - the UI is being updated");
-    });
-};
-
-// getName is an asynchronous function that uses fetch() to make a GET request to the geonames API. This function takes three parameters, which are the base URL, the zip code we want, and the API key
-const getGeoname = async (geonamesURL, city, userName)=>{
-    const res = await fetch(`${geonamesURL}placename=${city}&username=${userName}`); // We set a variable to hold the fetch calls. And the await keyword is telling it not to go on to the next part until it has received the data it needs. This URL in the fetch is what will let us query the OpenWeatherMap API. I set it so that us zip codes are hard coded
-    try {
-        const data = await res.json();
-        //console.log(data); // Printing the data in the console received from the API, based on the city the user input
-        console.log("getGeoname (1): obtained data from the geonames API");
-        return data;
-    }  
-    catch(error) {
-        console.log("error", error);
-        // appropriately handle the error
-    }
-};
 
 // Function that updates dynamically with: City, Country is ___ days away
 document.getElementById('dateInput').addEventListener('change', getDate); // Listen for the change of the date input
@@ -103,10 +130,13 @@ function getDate() {
     */
 };
 
+
 const weatherForecast = async (city, weatherBaseURL, weatherAPI)=>{
+    console.log("weatherForecast: fetching data");
     const res = await fetch(`${weatherBaseURL}city=${city}&key=${weatherAPI}`); // We set a variable to hold the fetch calls. And the await keyword is telling it not to go on to the next part until it has received the data it needs. This URL in the fetch is what will let us query the OpenWeatherMap API. I set it so that US zip codes are hard coded
     try {
         const data = await res.json();
+        console.log("weatherForecast: received data");
         // console.log("The data is: ", data);
         // console.log("weatherForecast (1): obtained data from the weatherbit API");
         // console.log("The weather tommorow will be: ", data["data"][0].weather["description"]);
@@ -117,41 +147,43 @@ const weatherForecast = async (city, weatherBaseURL, weatherAPI)=>{
         // appropriately handle the error
     }
 };
+/*
 // Testing the function weatherForecast: 
 // weatherForecast("Raleigh", weatherBaseURL, weatherAPI);
 
-
-
 const getPixaImages = async (city, pixaBaseURL, pixaAPI)=>{
-    const res = await fetch(`${pixaBaseURL}key=${pixaAPI}&q=${city}&image_type=photo&per_page=3`); // We set a variable to hold the fetch calls. And the await keyword is telling it not to go on to the next part until it has received the data it needs. This URL in the fetch is what will let us query the OpenWeatherMap API. I set it so that US zip codes are hard coded
+    const res = await fetch(`${pixaBaseURL}key=${pixaAPI}&q=${city}&image_type=photo`); // We set a variable to hold the fetch calls. And the await keyword is telling it not to go on to the next part until it has received the data it needs. This URL in the fetch is what will let us query the OpenWeatherMap API. I set it so that US zip codes are hard coded
     try {
-        const data = await res.json();
-        console.log("The pixabay data is: ", data["hits"][0].pageURL);
+        const imageData = await res.json();
+        console.log("The pixabay data is: ", imageData["hits"][0].webformatURL);
         // console.log("weatherForecast (1): obtained data from the weatherbit API");
         // console.log("The weather tommorow will be: ", data["data"][0].weather["description"]);
-        return data;
+        return imageData;
     }  
     catch(error) {
         console.log("error", error);
         // appropriately handle the error
     }
+    // document.getElementById('image').src = imageData["hits"][0].webformatURL;
 };
 // Testing the function getPixaImages: 
 getPixaImages("Raleigh", pixaBaseURL, pixaAPI)
+*/
 
 // Updating the UI of the app dynamically - Note that  each Promise must be resolved successfully before we can update the UI (i.e., before we can call updateUI())
 const updateUI = async () => {
+    console.log("updateUI (0): calling backend for data");
     const request = await fetch('http://localhost:8080/all');
     try{
       const allData = await request.json();
-      console.log("/all (1): once the request finishes, then we get all of the data from the get function in index.js", allData);
+      console.log("updateUI (1): once the request finishes, then we get all of the data from the get function in index.js", allData);
       document.getElementById('city').innerHTML = allData.city; // country
       document.getElementById('country').innerHTML = allData.country; // country
       document.getElementById('date').innerHTML = allData.latitude; // latitude
       document.getElementById('content').innerHTML = allData.longitude; // longitude
   
       //document.getElementById('daysUntil').innerHTML = allData.daysUntilTravel; // longitude
-      console.log("/all (2): Put the data that we want into the DOM");
+      console.log("updateUI (2): Put the data that we want into the DOM");
     }catch(error){
       console.log("error", error);
     }
--- src/client/js/buttonHandler.js
+++ src/client/js/buttonHandler.js
@@ -3,7 +3,10 @@ import { getWeather } from "./app"
 
 function buttonHandler(event){
     // Added an event listener with a callback called performAction. So that when I click the generate button it will perform this action
-    document.getElementById('generate').addEventListener('click', performAction(event)); 
+    //document.getElementById('generate').addEventListener('click', performAction(event)); 
+    console.log("buttonHandler (0): entered");
+    performAction(event);
+    console.log("buttonHandler (1): completed");
 };
 
 // a list of exported variables
